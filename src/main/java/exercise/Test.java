c0b739497e0ec66ed2714315ec396167af5a4c0c
Neha Singhal <neha.singhal@tothenew.com>
2017-07-15 17:45:06 +0530

    update files

---
+++ src/main/java/exercise/Test.java
@@ -0,0 +1,18 @@
+package exercise;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Shreya on 7/5/2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+
+        Triangle triangle = (Triangle)applicationContext.getBean("triangle");
+        triangle.draw();
+
+    }
+}
