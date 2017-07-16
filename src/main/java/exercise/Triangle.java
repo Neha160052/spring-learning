78257c275d569bb02c878b5810ab7517b29396c1
Neha Singhal <neha.singhal@tothenew.com>
2017-07-16 12:07:18 +0530

    simple Bean Defination inheritance example

--- src/main/java/exercise/Triangle.java
+++ src/main/java/exercise/Triangle.java
@@ -10,12 +10,10 @@ import java.util.List;
 /**
  * Created by neha on 15/7/17.
  */
-public class Triangle implements ApplicationContextAware, BeanNameAware{
+public class Triangle extends shape{
     private Point point0;
     private Point point1;
     private Point point2;
-    private ApplicationContext context;
-    private String beanName;
 
     public Point getPoint0() {
         return point0;
@@ -41,33 +39,12 @@ public class Triangle implements ApplicationContextAware, BeanNameAware{
         this.point2 = point2;
     }
 
-    public ApplicationContext getContext() {
-        return context;
-    }
-
-    public String getBeanName() {
-        return beanName;
-    }
-
     @Override
     public String toString() {
         return "Triangle{" +
                 "point0=" + point0 +
                 ", point1=" + point1 +
                 ", point2=" + point2 +
-                ", context=" + context +
-                ", beanName='" + beanName + '\'' +
                 '}';
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.context = applicationContext;
-    }
-
-    @Override
-    public void setBeanName(String name) {
-        System.out.println("bean name : " + name);
-        this.beanName = name;
     }
 }
