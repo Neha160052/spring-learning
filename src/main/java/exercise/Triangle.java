d5674bca64fe59f37a83745142e144083a2d6cda
Neha Singhal <neha.singhal@tothenew.com>
2017-07-16 09:59:13 +0530

    initialize collection (use of collection example)

--- src/main/java/exercise/Triangle.java
+++ src/main/java/exercise/Triangle.java
@@ -1,43 +1,26 @@
 package exercise;
 
+import java.util.List;
+
 /**
  * Created by neha on 15/7/17.
  */
 public class Triangle extends shape {
-    private Point point0;
-    private Point point1;
-    private Point point02;
 
-    public Point getPoint0() {
-        return point0;
+
+   private List<Point> points;
+
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPoint0(Point point0) {
-        this.point0 = point0;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
-    public Point getPoint1() {
-        return point1;
-    }
-
-    public void setPoint1(Point point1) {
-        this.point1 = point1;
-    }
-
-    public Point getPoint02() {
-        return point02;
-    }
-
-    public void setPoint02(Point point02) {
-        this.point02 = point02;
-    }
-
-    @Override
-    public String toString() {
-        return "Triangle{" +
-                "point0=" + point0 +
-                ", point1=" + point1 +
-                ", point02=" + point02 +
-                '}';
+    public void draw(){
+        for (Point point : points){
+            System.out.println("point = " + point);
+        }
     }
 }
