06e4fb88cc55c0105ff95b89b4e765eabff6483a
Neha Singhal <neha.singhal@tothenew.com>
2017-07-16 12:13:30 +0530

    use collection and bean defination inhertance exampe

--- src/main/java/exercise/Triangle.java
+++ src/main/java/exercise/Triangle.java
@@ -11,9 +11,19 @@ import java.util.List;
  * Created by neha on 15/7/17.
  */
 public class Triangle extends shape{
+    private List<Point> points;
     private Point point0;
     private Point point1;
     private Point point2;
+
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
 
     public Point getPoint0() {
         return point0;
@@ -39,6 +49,13 @@ public class Triangle extends shape{
         this.point2 = point2;
     }
 
+
+    public void draw(){
+        System.out.println("############# List of Point ###############");
+        for (Point point : points){
+            System.out.println("Point : " + point);
+        }
+    }
     @Override
     public String toString() {
         return "Triangle{" +
